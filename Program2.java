e527905cdfdde9a143bfe3323aa2cd3329f688c1
Goldfootsoldier <92195890+Goldfootsoldier@users.noreply.github.com>
2025-12-31 01:14:27 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Program2
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter Your Name: ");
+String name = sc.next();
+System.out.println("Enter Your Age: ");
+int age = sc.nextInt();
+System.out.println("Your Name is " + name + " and your age is " + age);
+}
+}
---
+++ Program3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+class Program3
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter two numbers to find the greatest number");
+System.out.println("Enter first number");
+int a = sc.nextInt();
+System.out.println("Enter second number");
+int b = sc.nextInt();
+System.out.println("The greatest number is " + Math.max(a,b));
+}
+}
+
