f1c68430e627bdd466ebbe40fd5e1aa0083bb3a2
Avaneesh Arunkumar <92195890+Goldfootsoldier@users.noreply.github.com>
2026-01-19 11:35:23 +0530

    Uploading Program and Class file for Experiment 1

---
+++ Experiment-1/Program5.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Program5
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a byte:");
+byte a = sc.nextByte();
+System.out.println("Enter a short:");
+short b = sc.nextShort();
+System.out.println("Enter an integer:");
+int c = sc.nextInt();
+System.out.println("Enter a long:");
+long d = sc.nextLong();
+System.out.println("Enter a float:");
+float e = sc.nextFloat();
+System.out.println("Enter a double:");
+double f = sc.nextDouble();
+System.out.println("Enter a character");
+char g = sc.next().charAt(0);
+System.out.println("Enter a String:");
+String h = sc.next();
+System.out.println("Enter a boolean:");
+boolean i = sc.nextBoolean();
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+System.out.println(i);
+}
+}
+
