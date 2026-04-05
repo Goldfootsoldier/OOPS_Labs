9c34380bcba36d342d99c2acf9804a576f871123
Avaneesh Arunkumar <92195890+Goldfootsoldier@users.noreply.github.com>
2026-04-05 14:23:16 +0530

    Upload Program and Class Files of Experiment-10

---
+++ Experiment-10/Program19.java
@@ -0,0 +1,29 @@
+class Student {
+private String name;
+private int age;
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public int getAge() {
+return age;
+}
+public void setAge(int age) {
+if (age > 0) {
+this.age = age;
+} else {
+System.out.println("Invalid age!");
+}
+}
+}
+public class Program19 {
+public static void main(String[] args) {
+Student student = new Student();
+student.setName("Avaneesh");
+student.setAge(20);
+System.out.println("Name: " + student.getName());
+System.out.println("Age: " + student.getAge());
+}
+}
